72a47217aaf0cfb12d6d6d4985dcd5cb5b10bccc
karlderkaefer <karlderkaefer@users.noreply.github.com>
2023-09-14 15:05:11 +0200

    fix: fix the modify checkbox function

--- src/check.ts
+++ src/check.ts
@@ -1,23 +1,41 @@
+import * as core from '@actions/core'
+
 export enum CheckboxAction {
   Check = 'check',
   Uncheck = 'uncheck'
 }
 
+/**
+ * Determines the new checkbox state based on the given action.
+ *
+ * @param action - The desired action to be taken (check or uncheck).
+ * @returns 'x' if action is to check, ' ' if action is to uncheck.
+ */
+function getCheckboxState(action: CheckboxAction): string {
+  return action === CheckboxAction.Check ? 'x' : ' '
+}
+
+/**
+ * Modifies the checkboxes in the given PR description based on the specified action.
+ *
+ * @param prDescription - The PR description containing markdown checkboxes.
+ * @param checkboxes - List of checkboxes to modify.
+ * @param action - The desired action (check or uncheck) to be taken on the specified checkboxes.
+ * @returns A modified PR description with the checkboxes checked or unchecked based on the action.
+ */
 export function modifyCheckboxes(
   prDescription: string,
   checkboxes: string[],
   action: CheckboxAction
 ): string {
-  const regex = /^- \[([xX ]*)\]\s*(.*)$/
-  const lines = prDescription.split('\n')
-  const newLines = lines.map(line => {
-    const match = regex.exec(line)
-    if (match !== null && checkboxes.includes(match[2].trim())) {
-      const newCheckboxState = action === CheckboxAction.Check ? 'x' : ' '
-      const newLine = `- [${newCheckboxState}] ${match[2]}`
-      return newLine
+  const regex = /-\s*\[([xX ]*)\]\s*(.*?)(?=\n|$)/g
+
+  return prDescription.replace(regex, (match, state, checkboxText) => {
+    if (checkboxes.includes(checkboxText.trim())) {
+      const newCheckboxState = getCheckboxState(action)
+      core.info(`${action} checkbox: ${checkboxText}`)
+      return `- [${newCheckboxState}] ${checkboxText}`
     }
-    return line
+    return match
   })
-  return newLines.join('\n')
 }
